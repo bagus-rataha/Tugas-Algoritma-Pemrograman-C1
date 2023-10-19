9278b9a9ee5104eed529f3840c69e90124a5837a
bagus-rataha <luckyxyzerian@gmail.co.id>
2023-10-20 01:26:20 +0700

    Ading code for reverse pyramid and combine 2 pyramid

--- Custom_Latih1_Diamond.py
+++ Custom_Latih1_Diamond.py
@@ -29,10 +29,33 @@ def pyramid():
             # print(i-1)
             baris += str(l)
             angka -= 1
-
+        
         # Menampilkan baris
         print(baris)
+        
+        
+    for i in range(tinggi-1, 0, -1):
+        baris = ''
 
-tinggi_piramida = 10  # Ubah tinggi piramida sesuai keinginan Anda
+        # Section Space
+        for j in range(tinggi - i):
+            baris += ' '
+
+        angka = 1
+        # Section Line Numbers
+        for k in range(1, i + 1): #123
+            baris += str(angka) #123
+            angka += 1 #234
+            
+        angka -= 2 #karna di awal sudah nambah 1 dan di akhir ditambah 1 lagi, supaya turun 2 angka sesuai urutan perlu dikurangin 2
+        
+        # Section Total Reverse Numbers Line
+        for l in range(i - 1, 0, -1):
+            # print(i-1)
+            baris += str(l)
+            angka -= 1
+        
+        # Menampilkan baris
+        print(baris)
 
 pyramid()

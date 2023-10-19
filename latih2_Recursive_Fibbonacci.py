0d12fd4ed2e0fa04d45e16d743b7c459521277f6
bagus-rataha <luckyxyzerian@gmail.co.id>
2023-10-20 00:34:22 +0700

    Penambahan fungsi input dinamis dan perubahan output agar lebih rapih

--- latih2_Recursive_Fibbonacci.py
+++ latih2_Recursive_Fibbonacci.py
@@ -3,12 +3,27 @@ def fibonacci (n):
     return [n]
 
   listSebelumN = fibonacci(n - 1)
-  angka1 = listSebelumN[-2] if len(listSebelumN) > 2 else 0
-  angka2 = listSebelumN[-1] if len(listSebelumN) > 2 else 1
+  angka1 = listSebelumN[-2] if len(listSebelumN) > 1 else 0 # Adjust to 1
+  angka2 = listSebelumN[-1] if len(listSebelumN) > 1 else 1 # Adjust to 1
 
   return listSebelumN + [angka1 + angka2]
 
-panjang = int(input('Masukkan panjang deret:'))
 
-# kita kurangin satu agar tidak kelebihan :D
-print(fibonacci(panjang - 1))
+def input_dynamic():
+  inputan = input("Enter numbers length : ")
+
+  if not inputan.isdigit():
+      print('Please Only Input Numbers!!!')
+      return input_dynamic()
+  
+  print("Calculating...")
+  list_fibo = fibonacci(int(inputan) - 1) #adding -1 to clean call function
+  string = ''
+  for index, number in enumerate(list_fibo):
+    string += f"| (" + str(index+1) + ".) " + str(number) +" \n" if (index+1) % 5 == 0 else f" | (" + str(index+1) + ".) " + str(number) +" " 
+  
+  string = string if string != '' else "There's no Fibonacci Numbers in This Range"
+  print(string)
+  
+  
+input_dynamic() #adjust moving -1

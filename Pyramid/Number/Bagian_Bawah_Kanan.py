afe18c4661f8b157f230896f2180989c204f8cf8
bagus-rataha <luckyxyzerian@gmail.co.id>
2023-10-23 01:29:10 +0700

    Penambahan Logic untuk kanan bawah

--- Pyramid/Number/Bagian_Bawah_Kanan.py
+++ Pyramid/Number/Bagian_Bawah_Kanan.py
@@ -28,22 +28,22 @@ def diamond(tinggi):
         baris = ''
 
         # Section Space
-        for j in range(tinggi - i):
-            baris += ' '
+        # for j in range(tinggi - i):
+        #     baris += ' '
 
         angka = 1
         # Section Line Numbers
-        for k in range(1, i + 1): #123
-            baris += str(angka) #123
-            angka += 1 #234
+        # for k in range(1, i + 1): #123
+        #     baris += str(angka) #123
+        #     angka += 1 #234
             
         angka -= 2 #karna di awal sudah nambah 1 dan di akhir ditambah 1 lagi, supaya turun 2 angka sesuai urutan perlu dikurangin 2
         
         # Section Total Reverse Numbers Line
-        # for l in range(i - 1, 0, -1):
-        #     # print(i-1)
-        #     baris += str(l)
-        #     angka -= 1
+        for l in range(i, 0, -1):
+            # print(i-1)
+            baris += str(l)
+            angka -= 1
         
         # Menampilkan baris
         print(baris)

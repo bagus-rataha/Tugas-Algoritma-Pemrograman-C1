18efc37ee5d1ec6124a8cdd248859237e226a7f5
Bintang Erlangga <93974579+BintangErlangga@users.noreply.github.com>
2023-11-02 16:33:30 +0700

    Create 6. Kalkulator Sederhana.py

---
+++ UTS/6. Kalkulator Sederhana.py
@@ -0,0 +1,47 @@
+# Kalkulator sederhana 
+
+# membuat fungsi operator + - / x
+
+def tambah (a,b):
+    return a + b
+
+def kurang(a,b):
+    return a - b 
+
+def bagi(a,b):
+    return a / b
+
+def kali(a,b):
+    return a * b\
+
+
+print(f"Selamat datang diKalkulator sederhana.Silahkan masukan angka!\n")
+
+# Input angka untuk kalkulator sederhana 
+
+angka1 = float(input("Masukan angka pertama :"))
+print(f'\nPiliih operator \n 1. + (tambah) \n 2. - (kurang)\n 3. / (bagi) \n 4. x (kali)')
+operator= input("\nMasukan operator yang anda inginkan :(+,-,/,x) ")
+angka2 = float(input("\nMasukan angka kedua :"))
+
+
+if operator == "1" :
+    hasil = tambah(angka1,angka2)
+    print(f"{angka1} + {angka2} = {hasil}")
+
+elif operator == "2" :
+    hasil = kurang(angka1,angka2)
+    print(f"{angka1} - {angka2} = {hasil}")
+
+elif operator == "3" :  
+    hasil = bagi(angka1,angka2)
+    print(f"{angka1} / {angka2} = {hasil}")
+
+elif operator == "4" :
+    hasil = kali(angka1,angka2)
+    print(f"{angka1} x {angka2} = {hasil}")
+
+else :
+    print("Masukan Angka yang benar dong !")
+
+print("\nTerima Kasih telah menggunakan layanan kami ")
